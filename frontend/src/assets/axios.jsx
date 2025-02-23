8240407f1d6add4e383d3156db804aad198d75a1
Max Kly <114592559+maxklymenko@users.noreply.github.com>
2025-02-23 14:03:12 +0000

    Add frontend url setup for Axios

--- frontend/src/assets/axios.jsx
+++ frontend/src/assets/axios.jsx
@@ -1,6 +1,6 @@
-import { backend_url } from "./backend_url"
+import { frontend_url } from "./frontend_url"
 import axios from "axios"
-const domain = backend_url || import.meta.env.VITE_DOMAIN
+const domain = frontend_url || import.meta.env.VITE_DOMAIN
 const axiosRequest = axios.create({
     baseURL: domain
 })
---
+++ frontend/src/assets/frontend_url.js
@@ -0,0 +1,1 @@
+export const frontend_url = 'https://my-weather-by-max-kly.netlify.app/'
