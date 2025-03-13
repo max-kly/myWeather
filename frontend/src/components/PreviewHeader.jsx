ec4842d5bba6d27c5fd8ccee1db239eb9564a8eb
Max Kly <114592559+maxklymenko@users.noreply.github.com>
2025-03-13 22:21:12 +0000

    Add icons as components, update components path, add close/add location functions

--- frontend/src/components/PreviewHeader.jsx
+++ frontend/src/components/PreviewHeader.jsx
@@ -1,36 +1,29 @@
-import CentralData from "./CentralData"
+import CentralData from "./data/CentralData"
 import { useWeather } from "./WeatherContext"
+import CloseIcon from "./icons/closeIcon"
+import SaveIcon from "./icons/SaveIcon"
+import { hidePreview, saveLocation } from "../utils/weatherPreview"
+import { useState } from "react"
 const PreviewHeader = ({ setShowLocationPreview }) => {
     const { weatherData } = useWeather()
+    const [isSaved, setIsSaved] = useState(localStorage.getItem(weatherData.location.name + ',' + weatherData.location.country) ? true : false)
     return (
         <div className="header">
-            <button id="closePreview" className="closePreview" onClick={(e) => {
-                e.currentTarget.parentElement.parentElement.classList.add('hidden')
-                setTimeout(() => {
-                    setShowLocationPreview(false)
-                }, 1000);
-            }}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-x-lg" viewBox="0 0 16 16">
-                    <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
-                </svg>
+            <button id="closePreview" className="closePreview" onClick={() => hidePreview(setShowLocationPreview)}>
+                <CloseIcon />
             </button>
             <CentralData weatherData={weatherData}></CentralData>
-            <button id="saveLoc" className="" onClick={(e) => {
-                e.currentTarget.querySelector('span').innerHTML = 'Saved'
-                e.currentTarget.parentElement.parentElement.classList.add('hidden')
-                setTimeout(() => {
-                    setShowLocationPreview(false)
-                }, 1000);
-                const locationName = weatherData.location.name + ',' + weatherData.location.country
-                const locationData = weatherData.location.lat + ',' + weatherData.location.lon
-                localStorage.setItem(locationName, locationData)
-            }}>
-                <svg className='saveIcon' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                    <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5" />
-                    <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708z" />
-                </svg>
-                <span className="button-desc">Save</span>
-            </button>
+            {isSaved ? (
+                <button className="disabled" id="saveLoc" onClick={() => alert('This Location has been already saved to your list')}>
+                    <SaveIcon />
+                    <span className="button-desc">Saved</span>
+                </button>
+            ) : (
+                <button id="saveLoc" onClick={() => saveLocation(weatherData, setIsSaved, setShowLocationPreview)}>
+                    <SaveIcon />
+                    <span className="button-desc">Save</span>
+                </button>
+            )}
         </div>
     )
 }
