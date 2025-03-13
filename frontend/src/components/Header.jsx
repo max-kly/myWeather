e9e0056b9323bfaa188c0e6aea74bbdf62446823
Max Kly <114592559+maxklymenko@users.noreply.github.com>
2025-03-13 22:19:15 +0000

    Update components path and add icons as components

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react"
-import CentralData from "./CentralData"
-import SearchCities from "./SearchCities"
-import SavedLocations from "./SavedLocations"
+import CentralData from "./data/CentralData"
+import SearchCities from "./windows/SearchCities"
+import SavedLocations from "./windows/SavedLocations"
+import ListIcon from "./icons/ListIcon"
+import SearchIcon from "./icons/SearchIcon"
 const Header = ({ setShowLocationPreview, setPreviewWeatherData }) => {
     const [showList, setShowList] = useState(false)
     const [savedLocations, setSavedLocations] = useState({})
@@ -13,27 +15,19 @@ const Header = ({ setShowLocationPreview, setPreviewWeatherData }) => {
                 const storage = { ...localStorage }
                 setSavedLocations(storage)
             }}>
-                <span className="icon">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
-                        <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5" />
-                    </svg>
-                </span>
+                <ListIcon />
                 <span className="button-desc">Saved locations</span>
             </button>
             <CentralData></CentralData>
             <button onClick={() => {
                 setShowList(!showList)
             }}>
-                <span className="icon">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
-                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
-                    </svg>
-                </span>
+                <SearchIcon />
                 <span className="button-desc">Search location</span>
             </button>
             {showList ? <SearchCities setShowList={setShowList} setShowLocationPreview={setShowLocationPreview} setPreviewWeatherData={setPreviewWeatherData}></SearchCities> : null}
             {showLocations ? <SavedLocations setShowLocations={setShowLocations} savedLocations={savedLocations} /> : null}
-        </div>
+        </div >
     )
 }
 export default Header
